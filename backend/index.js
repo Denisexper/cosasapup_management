291648dbb07a9e34db1682f3f0294f0481599b1e
Denisexper <clasic.exper2023@gmail.com>
2025-10-22 21:42:14 -0600

    Fix: enviroments for deploy production

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,12 @@ import cors from "cors"
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 
 const port = process.env.PORT
 
